93f6ba010f14da380188e30bf72c4dad26c5221d
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-08-18 20:36:20 -0300

    continuando

--- frontend/src/components/Board/index.js
+++ frontend/src/components/Board/index.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { loadLists } from "./../../services/api";
 import List from "./../List";
 import { Container } from "./styles";
 
-const lists = loadLists();
+import BoardContext from './context'
+
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(from, to) {
+    console.log(from, to)
+  }
+
   return (
     <Container>
       {lists.map(list => (
-        <List key={list.title} data={list}/>
+        <List key={list.title} data={list} />
       ))}
-
     </Container>
   );
 }
