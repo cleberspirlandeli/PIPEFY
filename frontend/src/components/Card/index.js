a1c6bf195eb91d2f83e8dbaebddfd9a14d4f1824
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-08-18 17:08:58 -0300

    terminando frontend e iniciando drag and drop

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -2,17 +2,16 @@ import React from "react";
 
 import { Container, Label } from "./styles";
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>Fazer a migração completo de servidor</p>
-      <img
-        src="https://api.adorable.io/avatars/75/abott@adorable.png"
-        alt="avatar"
-      />
+      <p>{data.content}</p>
+      { data.user && (<img src={data.user} alt="avatar" />)}
     </Container>
   );
 }
--- frontend/src/components/List/styles.js
+++ frontend/src/components/List/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   padding: 0 15px;
   height: 100%;
   flex: 0 0 320px;
+  opacity: ${props => (props.done ? 0.6 : 1)};
 
   & + div {
     border-left: 1px solid rgba(0, 0, 0, 0.05);
@@ -16,22 +17,22 @@ export const Container = styled.div`
     height: 42px;
 
     h2 {
-        font-weight: 500;
-        font-size: 16px;
-        padding: 0 10px;
+      font-weight: 500;
+      font-size: 16px;
+      padding: 0 10px;
     }
 
     button {
-        height:42px;
-        width: 42px;
-        border-radius: 18px;
-        background: #3b5bfd;
-        border: 0;
-        cursor: pointer;
+      height: 42px;
+      width: 42px;
+      border-radius: 18px;
+      background: #3b5bfd;
+      border: 0;
+      cursor: pointer;
     }
   }
 
   ul {
-      margin-top: 30px;
+    margin-top: 30px;
   }
 `;
